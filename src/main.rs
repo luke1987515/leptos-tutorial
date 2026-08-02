d9acfe4a3bc69ee4986a29d7e4a3dbf10f2ddd94
Luke Chen <luke1987515@hotmail.com>
2026-08-02 10:09:13 +0800

    Add main.rs Button dual count

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use leptos::mount::mount_to_body;
 use leptos::prelude::*;
 
+#[component]
+fn App() -> impl IntoView {
+    // 1. 建立 count 狀態，初始值為 0
+    let (count, set_count) = signal(0);
+
+    view! {
+        <p>"Hello, world!"</p>
+        <button
+            on:click=move |_| {
+                // 修改狀態：加 1 (也可以用 set_count.set(3);)
+                *set_count.write() += 1;
+            }
+        >
+            "Click me: "
+            // 讀取狀態：傳遞 Signal 本身或閉包，才能觸發動態更新
+            {count}
+        </button>
+        <p>
+            "Double count: "
+            {move || count.get() * 2}
+        </p>
+    }
+}
+
 fn main() {
-    mount_to_body(|| {
-        view! {
-            <p>"Hello, world!"</p>
-        }
-    })
+    mount_to_body(App);
 }
